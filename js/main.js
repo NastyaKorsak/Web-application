7729d5fd65881a34a8b557b90905cbbb43caa102
NastyaKorsak <46604676+NastyaKorsak@users.noreply.github.com>
2019-01-14 14:49:10 +0300

    Login

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('#myBtn')on.('click', function() {
+		var username = document.forms.customerDate.elements.username.value;
+	
+   if(!username) {
+   	alert('Введите логин')
+   }
+
+	});
+
+});
